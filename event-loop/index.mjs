e801b758cb610d7ee8041ae5d1fe9d9594bbeff9
holazz <2418184580@qq.com>
2022-04-30 18:35:56 +0800

    chore: update event-loop

--- event-loop/index.mjs
+++ event-loop/index.mjs
@@ -1,36 +1,49 @@
-// process.nextTick 始终在 setTimeout 和 setImmediate 之前执行
-// 延迟 0 毫秒的 setTimeout 与 setImmediate 非常相似。 执行顺序取决于各种因素，但是它们都会在事件循环的下一个迭代中运行
+// macroTask: I/O setTimeout setInterval setImmediate requestAnimationFrame(browser)
+// microTask: process.nextTick Promise MutationObserver(browser)
 
-const bar = () => console.log('bar')
-
-const foo = () => {
-  console.log('foo')
-
-  setImmediate(() => console.log('setImmediate1'))
-  setTimeout(() => console.log('setTimeout1'), 0)
-  setImmediate(() => console.log('setImmediate2'))
-  setTimeout(() => console.log('setTimeout2'), 0)
-
-  process.nextTick(() => {
-    console.log('nextTick')
+function main() {
+  setTimeout(() => {
+    console.log(12)
+    Promise.resolve().then(() => {
+      console.log(13)
+    })
   })
 
-  new Promise(
-    resolve => resolve('promise')
-  ).then(console.log)
+  console.log(1)
 
-  bar()
+  setImmediate(() => console.log(10))
+
+  new Promise((resolve) => {
+    console.log(2)
+    resolve()
+    console.log(3)
+  }).then(() => {
+    process.nextTick(() => console.log(9))
+    Promise.resolve().then(() => {
+      console.log(6)
+      Promise.resolve().then(() => console.log(7))
+    })
+    console.log(5)
+    setTimeout(() => {
+      console.log(14)
+    })
+    setImmediate(() => console.log(11))
+  })
+
+  process.nextTick(() => console.log(8))
+
+  console.log(4)
 }
 
-foo()
+main()
 
-// 执行结果
-// foo
-// bar
-// promise
-// nextTick
-// setImmediate1
-// setImmediate2
-// setTimeout0
-// setTimeout1
-// setTimeout2
+// process.nextTick(() => console.log(1))
+// Promise.resolve().then(() => console.log(2))
+// Promise.resolve().then(() => {
+//   console.log(3)
+//   process.nextTick(() => console.log(4))
+//   Promise.resolve().then(() => console.log(5))
+// }).then(() => {
+//   console.log(6)
+// })
+// 输出 2 3 5 6 1 4
